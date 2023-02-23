98e6b90a93d4d8a8d88dfb139d36627fb772b638
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-02-22 22:25:33 -0300

    update code Javascript

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@ const formulario = document.querySelector('#novoItem');
 
 formulario.addEventListener("submit", (evento)=> {
     evento.preventDefault();
-    console.log(evento.target.elements['nome'].value);
-    console.log(evento.target.elements['quantidade'].value);
-
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
 })
+
+
+function criaElemento (nome, quantidade) {
+
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    const lista = document.getElementById("lista");
+
+    lista.appendChild(novoItem);
+}
 
